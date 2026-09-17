29d9da8e89df21bc59e86ca7f37764faef68f93e
Nenavath Vivek <viveknenavath96@gmail.com>
2026-09-17 20:00:23 +0530

    keep only three python projects

--- turtle_exercise_1.py
+++
@@ -1,9 +0,0 @@
-from turtle import Turtle,Screen
-s1=Screen()
-tom = Turtle()
-for _ in range(10):
-    tom.forward(10)
-    tom.penup()
-    tom.forward(10)
-    tom.pendown()
-s1.exitonclick()      
